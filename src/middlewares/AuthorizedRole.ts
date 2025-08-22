b45865f65c580539f2de230da3f9333a4b894acd
mernwithsabbir <programingwithsabbir@gmail.com>
2025-08-22 19:15:33 +0600

    add Authorized Role Middleware

---
+++ src/middlewares/AuthorizedRole.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import { tokenDataInterface } from "../types/token";
+
+export const authorizeRole = (roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user as tokenDataInterface; // jwt থেকে আসছে
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized! No user data found.",
+      });
+    }
+
+    if (!roles.includes(user.role)) {
+      return res.status(403).json({
+        success: false,
+        message: "Forbidden! You do not have permission.",
+      });
+    }
+
+    next();
+  };
+};
